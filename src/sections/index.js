10e7c4424d1bcd08804d1f7c471f615b4638dd90
Nitish Sharma <nit4642@gmail.com>
2024-06-12 13:42:41 +0530

    Commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import SpecialOffers from "./SpecialOffers";
+import CustomerReviews from "./CustomerReviews";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+
+export {
+    Hero,
+    PopularProducts,
+    Services,
+    Subscribe,
+    SuperQuality,
+    SpecialOffers,
+    CustomerReviews,
+    Footer
+}
